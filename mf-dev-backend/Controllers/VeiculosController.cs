dcd0b7d3cff6cb81e0db61b2d6be0fe7899f1264
Kayo Felipe <kayofelipelino2002@gmail.com>
2024-04-22 18:18:49 -0300

    Adicionando relatorio

--- mf-dev-backend/Controllers/VeiculosController.cs
+++ mf-dev-backend/Controllers/VeiculosController.cs
@@ -126,5 +126,29 @@ namespace mf_dev_backend.Controllers
 
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Relatorio(int? id){
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var veiculo = await _context.Veiculos.FindAsync(id);
+
+            if (veiculo == null)
+            {
+                return NotFound();
+            }
+
+            var consumos = await _context.Consumos.Where(c => c.VeiculoId == id).OrderByDescending(c => c.Data).ToListAsync();
+
+            decimal total = consumos.Sum(c => c.Valor);
+
+            ViewBag.veiculo = veiculo; 
+            ViewBag.total = total;
+
+            return View(consumos);
+        }
     }
 }
